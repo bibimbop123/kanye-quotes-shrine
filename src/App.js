158930fedadabaa87386a006b30d97be83481f21
briankim <brianhkim13@gmail.com>
2023-09-10 13:51:52 -0500

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./index.css"; // import your CSS file here
 import Kobe from "./assets/Kobe.mp4";
 import "./App.css";
 
@@ -18,14 +19,22 @@ function App() {
 
   return (
     <div className="App">
+      <br />
       <header className="App-header">
-        <h1>Kanye Says</h1>
-        <video src={Kobe} autoPlay controls />
-        <h2>...and I quote:</h2>
-        <h3>👇</h3>
-        <p>{quote}</p>
+        <h1 className="text-4xl font-bold mb-4">Kanye Says</h1>
+        <div className="video-container">
+          <video src={Kobe} autoPlay controls />
+        </div>
+        <h2 className="text-2xl font-bold mt-8">...and I quote:</h2>
+        <h3 className="text-xl font-bold">👇</h3>
+        <p className="quote">{quote}</p>
+        <button
+          className="bg-secondary text-primary px-4 py-2 rounded mt-4"
+          onClick={fetchQuote}
+        >
+          Get new quote
+        </button>
       </header>
-      <button onClick={fetchQuote}>Get new quote</button>
     </div>
   );
 }
---
+++ src/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#5465FF",
+        secondary: "#BEEF9E",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
